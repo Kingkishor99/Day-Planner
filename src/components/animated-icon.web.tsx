027093579e42f19d48a0f70de2923fb3584062d5
patilshivani01 <patilshivani2912@gmail.com>
2026-08-16 21:30:26 +0530

    test

--- src/components/animated-icon.web.tsx
+++ src/components/animated-icon.web.tsx
@@ -1,6 +1,6 @@
 import { Image } from 'expo-image';
 import { StyleSheet, View } from 'react-native';
-import Animated, { Keyframe, Easing } from 'react-native-reanimated';
+import Animated, { Easing, Keyframe } from 'react-native-reanimated';
 
 import classes from './animated-icon.module.css';
 const DURATION = 300;
@@ -8,6 +8,7 @@ const DURATION = 300;
 export function AnimatedSplashOverlay() {
   return null;
 }
+// 
 
 const keyframe = new Keyframe({
   0: {
